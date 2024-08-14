11d05058cbd440a9d5698d906e1da580f1a97951
code-iddih <idinyamwaya@gmail.com>
2024-08-14 13:04:51 +0300

    Add Class to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
+import mealLogo from '../assets/meal.png'; // Adjust the path based on your structure
 
 function Navbar() {
   return (
     <nav className="navbar">
+      <div className="logo-container">
+        <img src={mealLogo} alt="Meal Logo" className="navbar-logo" />
+      </div>
       <ul>
         <li><Link className="navbar-link" to="/">Home</Link></li>
         <li><Link className="navbar-link" to="/categories">Categories</Link></li>
