16ede2d0442264fb3fd073c32ed0092d1cac784a
code-iddih <idinyamwaya@gmail.com>
2024-08-14 20:21:43 +0300

    AAdds Welcome message

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from './AuthContext';
 
 function Home() {
+    const { user } = useContext(AuthContext);
+
     return (
-      <div>
-        <h1>Welcome to Meal Master</h1>
-        <p>Explore various meal categories and recipes.</p>
-      </div>
+        <div>
+            <h1>Welcome {user?.name}!</h1>
+            <img src={user?.profilePicture} alt="Profile" className="profile-icon" />
+            <p>Explore various meal categories and recipes.</p>
+        </div>
     );
-  }
-  
-  export default Home;
-  
+}
+
+export default Home;
