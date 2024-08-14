280d8359752dc8d2985058672e617209b8eb47fb
code-iddih <idinyamwaya@gmail.com>
2024-08-14 15:17:12 +0300

    Updates the Login Handle

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,16 +1,15 @@
 import React, { useContext } from 'react';
-import { Route, Navigate } from 'react-router-dom';
-import { AuthContext } from '../AuthContext';
+import { Navigate } from 'react-router-dom';
+import { AuthContext } from './AuthContext';
 
-const ProtectedRoute = ({ element: Element, ...rest }) => {
-  const { isAuthenticated } = useContext(AuthContext);
+const ProtectedRoute = ({ children }) => {
+  const { isLoggedIn } = useContext(AuthContext);
 
-  return (
-    <Route
-      {...rest}
-      element={isAuthenticated ? <Element /> : <Navigate to="/login" />}
-    />
-  );
+  if (!isLoggedIn) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
 };
 
 export default ProtectedRoute;
