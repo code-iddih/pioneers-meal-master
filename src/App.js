14a80fde062034a610df034521647239ad8028ee
code-iddih <idinyamwaya@gmail.com>
2024-08-16 01:20:11 +0300

    Removes Search Navlink

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import Header from './components/Header';
 import Home from './components/Home';
 import CategoryList from './components/CategoryList';
 import CategoryDetail from './components/CategoryDetail';
-import Recipe from './components/Recipe';
+import RecipeList from './components/RecipeList';
 import UserProfile from './components/UserProfile';
 import MealPlan from './components/MealPlan';
 import Login from './components/Login';
-import Search from './components/Search';
 import FeedbackForm from './components/FeedbackForm';
 import { AuthProvider, ProtectedRoute } from './components/AuthContext';
 
@@ -29,10 +28,9 @@ const App = () => {
                   <Route path="/" element={<Home />} />
                   <Route path="/categories" element={<CategoryList />} />
                   <Route path="/categories/:id" element={<CategoryDetail />} />
-                  <Route path="/recipe/:id" element={<Recipe />} />
+                  <Route path="/recipes" element={<RecipeList />} />
                   <Route path="/profile" element={<UserProfile />} />
                   <Route path="/meal-plan" element={<MealPlan />} />
-                  <Route path="/search" element={<Search />} />
                   <Route path="/feedback" element={<FeedbackForm />} />
                 </Routes>
               </ProtectedRoute>
