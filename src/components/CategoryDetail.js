1926d9813433bf4376ac25b236dd5fd1c57a9e04
code-iddih <idinyamwaya@gmail.com>
2024-08-16 01:12:15 +0300

    Adds Loading Status

--- src/components/CategoryDetail.js
+++ src/components/CategoryDetail.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-// import './CategoryDetail.css';
+import './CategoryDetail.css';
 
 function CategoryDetail() {
   const { idCategory } = useParams();
